0ee10039fbbe0ae3eaf21be6bec064197d69d0c5
Elenazuokaite <elena.zuokaite@gmail.com>
2017-11-14 21:06:52 +0200

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+import { routing } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostFormComponent } from './posts/post-form/post-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    PostsComponent,
+    PostFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
